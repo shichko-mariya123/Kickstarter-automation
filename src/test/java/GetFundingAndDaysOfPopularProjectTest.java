d909cfb4c6d853e4879f1e112739f578bf393935
Mariya Shichko <m.shichko@a1qa.com>
2022-01-24 16:47:02 +0300

    connect application

---
+++ src/test/java/GetFundingAndDaysOfPopularProjectTest.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class GetFundingAndDaysOfPopularProjectTest extends BaseAndroidTest{
+
+    @Test
+    public void getFundingAndDaysOfPopularProjectTest(){
+
+    }
+}
